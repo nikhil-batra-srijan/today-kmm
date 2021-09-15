5868cb1ad3c344414af2cf12ce60c4fd2f8ab078
Kartik Saraf <kartik.saraf@srijan.net>
2021-09-15 17:23:08 +0530

    updated dependency version

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,19 +26,19 @@ kotlin {
         frameworkName = "shared"
         podfile = project.file("../iOSApp/Podfile")
     }
-    
+
     sourceSets {
         val commonMain by getting {
             dependencies {
                 //Network
-                implementation("io.ktor:ktor-client-core:${findProperty("version.ktor")}")
-                implementation("io.ktor:ktor-client-logging:${findProperty("version.ktor")}")
+                implementation("io.ktor:ktor-client-core:${Dependencies.ktor_version}")
+                implementation("io.ktor:ktor-client-logging:${Dependencies.ktor_version}")
                 //Coroutines
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${findProperty("version.kotlinx.coroutines")}")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Dependencies.coroutines_version}")
                 //Logger
                 implementation("io.github.aakira:napier:2.0.0")
                 //JSON
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${findProperty("version.kotlinx.serialization")}")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Dependencies.kotlinx_serialization_version}")
                 //Key-Value storage
                 //implementation("com.russhwolf:multiplatform-settings:0.7.7")
                 implementation("io.ktor:ktor-client-serialization:1.6.2")
@@ -48,14 +48,14 @@ kotlin {
         val androidMain by getting {
             dependencies {
                 //Network
-                implementation("io.ktor:ktor-client-okhttp:${findProperty("version.ktor")}")
+                implementation("io.ktor:ktor-client-okhttp:${Dependencies.ktor_version}")
             }
         }
 
         val iosMain by getting {
             dependencies {
                 //Network
-                implementation("io.ktor:ktor-client-ios:${findProperty("version.ktor")}")
+                implementation("io.ktor:ktor-client-ios:${Dependencies.ktor_version}")
             }
         }
         val commonTest by getting {
@@ -75,11 +75,22 @@ kotlin {
 }
 
 android {
-    compileSdk = (findProperty("android.compileSdk") as String).toInt()
+    compileSdk = Dependencies.compileSdk
 
     defaultConfig {
-        minSdk = (findProperty("android.minSdk") as String).toInt()
-        targetSdk = (findProperty("android.targetSdk") as String).toInt()
+        minSdk = Dependencies.minSdk
+        targetSdk =  Dependencies.targetSdk
     }
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+}
+
+object Dependencies {
+    const val android_gradle_plugin = "7.0.0"
+    const val minSdk = 21
+    const val compileSdk = 30
+    const val targetSdk = 30
+    const val coroutines_version = "1.5.1-native-mt"
+    const val ktor_version = "1.6.2"
+    const val kotlinx_serialization_version = "1.2.2"
+
 }
