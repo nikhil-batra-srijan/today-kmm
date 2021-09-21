d8bacb130c5fc73a0e79ef62d52543a69675b13b
Kartik Saraf <kartik.saraf@srijan.net>
2021-09-21 23:45:41 +0530

    sealed class -> interface

--- shared/src/commonMain/kotlin/com/mediacorp/newscorekmm/repository/SomeInterface.kt
+++ shared/src/commonMain/kotlin/com/mediacorp/newscorekmm/repository/SomeInterface.kt
@@ -1,6 +1,5 @@
 package com.mediacorp.newscorekmm.repository
 
-sealed class SomeInterface {
-    data class ValueOne(val value: String) : SomeInterface()
-    data class ValueTwo(val value: String) : SomeInterface()
-}
+interface SomeInterface
+data class ValueOne(val value: String) : SomeInterface
+data class ValueTwo(val value: String) : SomeInterface
