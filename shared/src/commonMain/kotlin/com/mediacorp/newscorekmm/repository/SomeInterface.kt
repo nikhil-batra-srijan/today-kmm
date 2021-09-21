c246053c6a468d1133eff5116030577b055eef97
Kartik Saraf <kartik.saraf@srijan.net>
2021-09-21 23:37:53 +0530

    Update SomeInterface.kt

--- shared/src/commonMain/kotlin/com/mediacorp/newscorekmm/repository/SomeInterface.kt
+++ shared/src/commonMain/kotlin/com/mediacorp/newscorekmm/repository/SomeInterface.kt
@@ -1,5 +1,6 @@
 package com.mediacorp.newscorekmm.repository
 
-abstract class SomeInterface
-data class ValueOne(val value: String) : SomeInterface()
-data class ValueTwo(val value: String) : SomeInterface()
+sealed class SomeInterface {
+    data class ValueOne(val value: String) : SomeInterface()
+    data class ValueTwo(val value: String) : SomeInterface()
+}
