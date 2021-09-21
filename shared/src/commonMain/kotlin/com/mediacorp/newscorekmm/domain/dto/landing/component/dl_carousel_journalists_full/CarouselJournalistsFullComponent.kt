aa802912d67a54f3784fa13cc424728db41df0ec
Kartik Saraf <kartik.saraf@srijan.net>
2021-09-21 22:44:10 +0530

    Update CarouselJournalistsFullComponent.kt

--- shared/src/commonMain/kotlin/com/mediacorp/newscorekmm/domain/dto/landing/component/dl_carousel_journalists_full/CarouselJournalistsFullComponent.kt
+++ shared/src/commonMain/kotlin/com/mediacorp/newscorekmm/domain/dto/landing/component/dl_carousel_journalists_full/CarouselJournalistsFullComponent.kt
@@ -3,12 +3,13 @@ package com.mediacorp.newscorekmm.domain.dto.landing.component.dl_carousel_journ
 import com.mediacorp.newscorekmm.domain.dto.landing.component.LandingPageComponent
 import com.mediacorp.newscorekmm.domain.dto.landing.component.common.ComponentDetailStoryItem
 import com.mediacorp.newscorekmm.domain.dto.landing.component.common.CtaData
+import com.mediacorp.newscorekmm.domain.dto.landing.component.common.JournalistCarouselItem
 import com.mediacorp.newscorekmm.domain.dto.landing.component.common.TitleData
 
 data class CarouselJournalistsFullComponent(
     val uuid: String,
     val title: TitleData,
     val ctaData: CtaData,
-    val storyList: List<ComponentDetailStoryItem>,
+    val storyList: List<JournalistCarouselItem>,
     val isDarkMode: Boolean = false
 ):LandingPageComponent
